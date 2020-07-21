c1db7d3e029419f47f5f24eaa80bb4eaee6eeac3
Jeremy Cady <jeremy@mx103.com>
2020-07-21 06:27:01 -0500

    Added pagination links and capability

--- js/script.js
+++ js/script.js
@@ -16,7 +16,8 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
-
+const studentList = document.querySelectorAll('li');
+const countPerPage = 10;
 
 
 
@@ -34,17 +35,70 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+const showPage = (list, page) => {
+   const startIndex = (page * countPerPage) - countPerPage;
+   const endIndex = page * countPerPage;
+   const ul = document.querySelector('ul');
 
-
-
+   ul.innerHTML = '';
+  
+   for (let i=0; i<list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         const li = list[i];
+         ul.appendChild(li);
+      }
+   }
+}
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+const appendPageLinks = (list) => {
+   const numberOfPages = Math.ceil(list.length / countPerPage)
+   const pageDiv = document.querySelector('.page');
+   const paginationDiv = document.createElement('div');
+   const ul = document.createElement('ul');
 
+   paginationDiv.className = 'pagination';
+   pageDiv.appendChild(paginationDiv);
+   paginationDiv.appendChild(ul);
 
+   for (let i=1; i<=numberOfPages; i++) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
 
+      if (i === 1) {
+         a.className = 'active';
+      }
 
+      a.href = '#';
+      a.textContent = `${i}`;
+      ul.appendChild(li);
+      li.appendChild(a);
+   }
+}
+
+showPage(studentList, 1);
+appendPageLinks(studentList);
+
+document.addEventListener('click', (event) => {
+   if (event.target.tagName === 'A') {
+      const currentActive = document.querySelector('.active');
+      const links = document.querySelectorAll('a');
+      const link = event.target;
+      const page = link.textContent;
+
+      currentActive.className = '';
+
+      showPage(studentList, page);
+      
+      for (let i=1; i<=links.length; i++) {
+         if (i == page) {
+            link.className = 'active';
+         }
+      }
+   }
+});
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
